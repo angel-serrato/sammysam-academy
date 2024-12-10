3fca82623c0cfe4d487ef4ff23ee546069c18305
Angel Serrato <serratoarias@yahoo.com>
2024-12-10 15:12:07 -0500

    Base info added

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://angel-serrato.github.io/sammysam-academy',
+	base: '/sammysam-academy/',
 	integrations: [mdx(), sitemap()],
 });
